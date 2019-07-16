a9d4737f58d4d950b66c6b45ab4436a9f9bed85e
bor9sik <asdf514asdf514@gmail.com>
2019-07-16 12:49:40 +0300

    hw2 added8

--- hw-02/task-8.js
+++ hw-02/task-8.js
@@ -1,6 +1,6 @@
 function isLoginValid(login) {
     if (login.length >= 4 && login.length <= 16) {
-        console.log('Отличный логин');
+        // console.log('Логин успешно добавлен');
         return true;
     } else {
         console.log('Ошибка! Логин должен быть от 4 до 16 символов');
@@ -8,5 +8,26 @@ function isLoginValid(login) {
     }
 }
 
+const isLoginUnique = function (allLogins, login) {
+    if (allLogins.includes(login)) {
+        console.log('Такой логин уже используэться!!!');
+        return true;
+    } else {
+        console.log('Логин успешно добавлен');
+        return false;
+    }
+};
 
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+const addLogin = function (allLogins, login) {
+    if (isLoginValid(login)) {
+        (isLoginUnique(allLogins, login))
+    }
+};
+
+
+addLogin(logins, 'Ajax'); // 'Логин успешно добавлен!'
+addLogin(logins, 'robotGoogles'); // 'Такой логин уже используется!'
+addLogin(logins, 'Zod'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+addLogin(logins, 'jqueryisextremelyfast'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
